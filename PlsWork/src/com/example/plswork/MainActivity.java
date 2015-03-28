9dc43dfdbc336e429f3d103eafa88ccd874290fd
hussien4321 <user@10.8.104.180>
2015-03-28 18:49:22 +0000

    ADDED WORKING MOVING DEMO

--- PlsWork/src/com/example/plswork/MainActivity.java
+++ PlsWork/src/com/example/plswork/MainActivity.java
@@ -1,16 +1,31 @@
 package com.example.plswork;
 
 import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends ActionBarActivity {
 
+	Button but;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		but = (Button) this.findViewById(R.id.STARTGAME);
+		but.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+
+				Intent i = new Intent(getApplicationContext(), MainActivity2.class);
+				startActivity(i);
+			}
+		});
 	}
 
 	@Override
